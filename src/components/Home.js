7280fdf62eb7472833eea9fc8b9ad3b8710afe6f
dhanush <dhanushg25@gmail.com>
2023-08-13 19:07:56 +0530

    push not working

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div >
         <div className='h-screen w-full'>
-          <h1 className='h-full w-full flex justify-center items-center text-white text-3xl'>homepage</h1>
+          <h1 className='h-full w-full flex   justify-center items-center text-white text-3xl'>homepage</h1>
         </div>
         <div id="ctus" className='pb-96'> 
         <h1 className='text-stone-50'>Contact Us Form</h1>
